c3e0ef97f9756767236958e0e1c4d03d95940ac7
rajeshkalaiselvam <101263005+rajeshkalaiselvam@users.noreply.github.com>
2023-08-01 13:47:56 +0530

    zoom

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 'VgSZyqfsTLmy296g7i6sZw');
+define('CLIENT_SECRET', 'EbeivOppxQNeXp7pDiJtcnlmNfmEO899');
+define('REDIRECT_URI', 'https://projectreview.online/callback.php');
